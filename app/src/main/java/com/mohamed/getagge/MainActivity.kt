24f8f99c2aa26727a41c8909b8b6d2461527baa7
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2022-10-06 02:41:22 +0200

    finish the app2

--- app/src/main/java/com/mohamed/getagge/MainActivity.kt
+++ app/src/main/java/com/mohamed/getagge/MainActivity.kt
@@ -2,10 +2,22 @@ package com.mohamed.getagge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val yearOfBirthET = findViewById<EditText>(R.id.yearOfBirth_EditText)
+        val ageTV = findViewById<TextView>(R.id.age_textView)
+        val calculateBtn = findViewById<Button>(R.id.calculate_button)
+
+        calculateBtn.setOnClickListener {
+            val yOB = yearOfBirthET.editableText.toString()
+            val age = 2022 - yOB.toInt();
+            ageTV.text = age.toString()
+        }
     }
 }
